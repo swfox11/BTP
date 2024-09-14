f4288b2f4b6a9995673708dfcd556b6273312f41
OptimalAches <sachanakshat09@gmail.com>
2024-09-14 20:33:30 +0530

    updated navbar

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
     return (
       <footer className='bg-black text-white flex justify-center items-center h-16 px-8 py-8 mt-auto'>
-        <span className='text-center font-semibold'>Copyright &copy; {currentYear} <span>Oceanic</span><span className="text-[#48bf91]">Edge</span> - All rights reserved</span>
+        <div className='font-semibold'>Copyright &copy; {currentYear} <span className='font-bold font-serif'><span>Oceanic</span><span className="text-[#48bf91]">Edge</span></span> - All rights reserved</div>
       </footer>
     )
 }
--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,106 +1,87 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import { useState } from 'react'
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+
+  const [showdropdown, setshowdropdown] = useState(false)
+  const [showdropdown2, setshowdropdown2] = useState(false)
+
+  const pathname = usePathname();
+
   return (
-    <nav className='bg-black text-white flex justify-between items-center md:h-16 flex-col md:flex-row px-4'>
+    <nav className='bg-black text-white flex flex-col justify-between items-center md:h-16 md:flex-row px-3'>
 
-      <div className='logo font-bold text-lg flex justify-center items-center gap-2'>
+      <div className='flex gap-16 justify-center items-center'>
 
-        <Link href={'/'}><img src="/favicon.gif" alt="logo" width={35} /> </Link>
-        <Link href={'/'}><div><span>Oceanic</span><span className="text-[#48bf91]">Edge</span></div> </Link>
+        <div className='flex gap-1 text-2xl items-center justify-center'>
+          <img src="/favicon.gif" alt="logo" width={35} />
+          <span className='font-bold font-serif'><span>Oceanic</span><span className="text-[#48bf91]">Edge</span></span>
+        </div>
+
+        <div className="flex gap-8 font-bold text-lg items-center justify-center">
+          <Link className={pathname === '/' ? 'active' : ''} href="/">Home</Link>
+          <Link className={pathname === '/about' ? 'active' : ''} href="/about">About</Link>
+        </div>
 
       </div>
 
-      <div className="flex gap-4 text-white font-bold items-center justify-center">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+
+      <div className="flex gap-3 items-center justify-center">
 
         <div>
-          <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Department<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button onClick={() => { setshowdropdown(!showdropdown) }} onBlur={() => { setTimeout(() => { setshowdropdown(false) }, 100) }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Department (IIT KGP)<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
           </svg>
           </button>
 
           {/* Dropdown menu  */}
-          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+          <div id="dropdown" className={`${showdropdown ? "" : "hidden"} absolute right-[213px] top-[60px] bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+            <ul className="py-2 text-sm font-semibold text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Faculty</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Faculty</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Location</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Location</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Lab Facilities</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Lab Facilities</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Gallery</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Gallery</Link>
               </li>
             </ul>
           </div>
         </div>
 
         <div>
-          <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Student Resources<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button onClick={() => { setshowdropdown2(!showdropdown2) }} onBlur={() => { setTimeout(() => { setshowdropdown2(false) }, 100) }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Student Resources<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
           </svg>
           </button>
 
           {/* Dropdown menu  */}
-          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+          <div id="dropdown" className={`${showdropdown2 ? "" : "hidden"} absolute right-[14px] top-[60px] bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+            <ul className="py-2 text-sm font-semibold text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Terminologies</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Terminologies</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Basic Calculations</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Basic Calculations</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Global Fleet 2024</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Global Fleet 2024</Link>
               </li>
               <li>
-                <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Publications</Link>
+                <Link href="#" className="block px-4 py-2 hover:bg-[#48bf91] dark:hover:bg-gray-600 dark:hover:text-white">Publications</Link>
               </li>
             </ul>
           </div>
         </div>
 
-
-
-
-
       </div>
-
-
-
-      {/* <div className='relative flex flex-col gap-4 md:block'>
-
-                {session && <><button onClick={() => { setshowdropdown(!showdropdown) }} onBlur={()=>{setTimeout(()=>{setshowdropdown(false)}, 100)}} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 mx-4 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email}<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeLidth="2" d="m1 1 4 4 4-4" />
-                </svg>
-                </button>
-                    <div id="dropdown" className={`z-10 ${showdropdown ? "" : "hidden"} absolute left-[165px] bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                            <li>
-                                <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link href={`${session.user.name}`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Your Page</Link>
-                            </li>
-                            <li>
-                                <Link onClick={() => { signOut() }} href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </>}
-
-                {session && <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => { signOut() }}>Logout</button>}
-
-                {!session && <Link href={"/login"}><button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button></Link>}
-            
-            </div> */}
 
     </nav>
   )
