4e76b0716ca9cac1d0cde23265c6b18d3cbe4fa0
Swarnava Das <swarnava0802@gmail.com>
2024-11-26 20:02:30 +0530

    test

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const LocationPage = () => {
+  
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-800 mb-6 transition-transform transform hover:scale-105">
