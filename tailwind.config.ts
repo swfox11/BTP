98ef7de2fdc23ae28f393e60317ca9d0904f8239
OptimalAches <sachanakshat09@gmail.com>
2025-01-01 14:47:59 +0530

    reversed few changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,9 +36,7 @@ const config: Config = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    
   ],
 };
 export default config;
