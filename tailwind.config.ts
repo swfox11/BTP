5a8f295128f9f8411f83379235d5637ccf60c81f
OptimalAches <sachanakshat09@gmail.com>
2025-01-01 14:23:55 +0530

    updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // content: [
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //   './pages/**/*.{js,ts,jsx,tsx}', 
+  //   './components/**/*.{js,ts,jsx,tsx}', 
+  //   './app/**/*.{js,ts,jsx,tsx}', // If using the Next.js App directory
+  //   './layouts/**/*.{js,ts,jsx,tsx}', // If using custom layouts
+  //   './public/**/*.html', // For static HTML files
+    
+  // ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './pages/**/*.{js,ts,jsx,tsx}', 
-    './components/**/*.{js,ts,jsx,tsx}', 
-    './app/**/*.{js,ts,jsx,tsx}', // If using the Next.js App directory
-    './layouts/**/*.{js,ts,jsx,tsx}', // If using custom layouts
-    './public/**/*.html', // For static HTML files
-    
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html", // For static HTML files
   ],
   theme: {
     extend: {
